315d03cbe983fc037cd053f5c83cd1c34c2c9c61
DashaSuhotskaya <d.suhotskaya@gmail.com>
2026-02-06 00:28:29 +0300

    Delete project

--- AffArts_vanillaJS/js/components/getProducts.js
+++
@@ -1,5 +0,0 @@
-export default async function getProducts() {
-    const response = await fetch("./data/data.json");
-    const products = await response.json();
-    return products;
-  }
--- AffArts_vanillaJS/js/components/updateFilterCounts.js
+++
@@ -1,7 +0,0 @@
-export default function updateFilterCounts(checkboxes, products) {
-    checkboxes.forEach(checkbox => {
-        const countElement = checkbox.closest('.custom-checkbox').querySelector('.custom-checkbox__count')
-        let count = products.filter(product => product.type.includes(checkbox.value)).length;;
-        countElement.textContent = count;
-    });
-}
